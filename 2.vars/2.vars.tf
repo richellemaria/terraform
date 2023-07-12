6e80b6105e5342ff4afa2148f98b3000abd523fa
richellemaria <rishdsouza@gmail.com>
2023-07-12 16:12:49 +0530

    auto push

--- 2.vars/2.vars.tf
+++ 2.vars/2.vars.tf
@@ -24,3 +24,19 @@ variable "example_list"{
 output "example_list_op"{
     value = "welcome to ${var.example_list[1]} with ${var.example_list[0]} this is ${var.example_list[2]} and current topic is ${var.example_list [3]}"
 }
+
+# map variable
+
+variable "example_map"{
+    default = {
+        Batch = "B54"
+        Training = "DevopsAWS"
+        Mode = "online"
+
+    }
+}
+
+output "example_map_op"{
+    value = "We are learning  ${var.example_map["Training"]} and this is ${var.example_map["Batch"]} and it is ${var.example_map["Mode"]}"
+}
+
