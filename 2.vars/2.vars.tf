e33b6169f95a059b281434c988af9c7434739478
richellemaria <rishdsouza@gmail.com>
2023-07-12 15:59:23 +0530

    auto push

--- 2.vars/2.vars.tf
+++ 2.vars/2.vars.tf
@@ -2,10 +2,25 @@ variable "sample"{
     default = "Terraform basics"
 }
 
-output "sample-op"{
+output "sample_op"{
    value = var.sample
 }
 
-output "sample-op-variable"{
+output "sample_op_variable"{
     value = "The value of variable is ${ var.sample}"
+}
+
+# list variable
+
+variable "example_list"{
+    default = [
+        "AWS";
+        "Devops",
+        54
+        "Terraform"
+    ]
+}
+
+output "example_list_op"{
+    value = "welcome to ${ var.example_list[1]} with ${var.example[0]} this is ${var.example[2]} and current topic is ${var.example_list[3]}"
 }
