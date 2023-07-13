a274e35cbe52ede1d5c05f9b6367bb13d644b3c6
richellemaria <rishdsouza@gmail.com>
2023-07-13 22:36:23 +0530

    auto push

--- 2.vars/sample.tf
+++ 2.vars/sample.tf
@@ -13,3 +13,11 @@ variable "state"{
 output "state_op"{
     value = "My state is ${var.state}"
 }
+
+variable "coldcity"{
+
+}
+
+output "cold_city_output"{
+    value = "The coldcity is ${var.coldcity}"
+}
