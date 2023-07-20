ee4d32aa8861d0b8470e2f15d8bf1464b4f38e21
richellemaria <rishdsouza@gmail.com>
2023-07-20 23:00:29 +0530

    auto push

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,8 +1,37 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "app_server" {
   ami           = "ami-02edefb7f078d80d3"
   instance_type = "t2.medium"
+  vpc_security_group_ids = ["aws_security_group.allow_allow.id"]
 
   tags = {
     Name = "Terraform"
+  }
+}
+
+
+#security group
+
+resource "aws_security_group" "allow_allow" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = 0.0.0.0/0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
