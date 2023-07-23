e643e565c1b74a883f372b371e934c136d6176d3
richellemaria <rishdsouza@gmail.com>
2023-07-23 16:08:59 +0530

    auto push

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -3,6 +3,21 @@ resource "aws_instance" "server" {
   instance_type = "t2.medium"
   vpc_security_group_ids = [var.sg]
 
+  provisioner "remote-exec" {
+
+    connection {
+     type     = "ssh"
+     user     = "centos"
+     password = "DevOps321"
+     host     = self.public_ip     #aws_instance.server.private_ip if provisioners will be outside the resource
+  }
+
+    inline = [
+      "ansible-pull -U https://github.com/richellemaria/ansible.git -e ENV=dev -e COMPONENT=frontend"
+    ]
+
+}
+
 }
 
 variable "sg"{}   #step 3 to use the information declare empty varibale and use it
