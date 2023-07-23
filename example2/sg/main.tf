d0e47cc59803fe907884d4fd074e0a92908396d1
richellemaria <rishdsouza@gmail.com>
2023-07-23 16:14:28 +0530

    auto push

--- example2/sg/main.tf
+++ example2/sg/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allows_all" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
