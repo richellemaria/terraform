6a349c782a37fc955c33e08b8fd2bc981da120ff
richellemaria <rishdsouza@gmail.com>
2023-07-23 19:58:11 +0530

    auto push

--- roboshop-setup/ec2/main.tf
+++ roboshop-setup/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_spot_instance_request" "spot_worker" {
   ami           = data.aws_ami.image_id.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allows_all.id]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "var.COMPONENT"
