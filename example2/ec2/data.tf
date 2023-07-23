452bad3f0ed1995e46195bcd2be3cf1ff3da4293
richellemaria <rishdsouza@gmail.com>
2023-07-23 12:36:03 +0530

    auto push

--- example2/ec2/data.tf
+++ example2/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image_id" {
-  executable_users = ["self"]
+  #executable_users = ["self"]
   most_recent      = true
   name_regex       = "ansible-lab-image"
   owners           = ["self"]
